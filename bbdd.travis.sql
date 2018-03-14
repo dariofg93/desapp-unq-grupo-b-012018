0b591f8c4b0ccf7e61f56836c25b7a2eb9ecc7da
Fabrizio Britez <Fabrizio Britez>
2018-03-14 14:15:17 -0300

    update .travis.yml

--- bbdd.travis.sql
+++
@@ -1,6 +0,0 @@
- start transaction;
-  DROP DATABASE IF EXISTS `egc-frontend`;
-  CREATE DATABASE `egc-frontend`; 
-  USE `egc-frontend`;
-  GRANT ALL ON `egc-frontend`.* TO 'user-frontend'@'localhost' IDENTIFIED BY 'us4r-front4nd';
-  COMMIT;
