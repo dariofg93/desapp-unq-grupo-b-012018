56a0bebc42d28cea561c50fd6d786f48893563d5
fbritez <fabri1108@gmail.com>
2018-03-18 17:15:25 -0300

    cambio de estructura del proyecto

---
+++ desapp-groupB-backend/src/test/modelTests/UserTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testSuccess() {
+		assertTrue(true);
+	}
+
+	public void testFailed() {
+		assertTrue(false);
+	}
+}
