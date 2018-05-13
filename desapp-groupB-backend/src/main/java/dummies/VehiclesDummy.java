714f8b7e4b7a91a051f5fb0db572d2475faf97be
dario93 <dariofg93@gmail.com>
2018-05-13 00:13:24 -0300

    antes de pull

--- desapp-groupB-backend/src/main/java/dummies/VehiclesDummy.java
+++ desapp-groupB-backend/src/main/java/dummies/VehiclesDummy.java
@@ -4,17 +4,18 @@ import model.builders.VehicleBuilder;
 import model.vehicle.Vehicle;
 import model.vehicleType.Car;
 import model.vehicleType.Scooter;
+import service.VehicleService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class VehiclesRepository {
+public class VehiclesDummy {
 
     private List<Vehicle> vehicles = new ArrayList<>();
     private VehicleBuilder builder = new VehicleBuilder();
+    private VehicleService service;
 
-    public VehiclesRepository(){
-
+    public VehiclesDummy(){
         Vehicle vehicle1 = builder.createVehicle()
                 .withDescription("Un auto muy veloz!")
                 .withPassengerCapacity(2)
@@ -32,5 +33,22 @@ public class VehiclesRepository {
         this.vehicles.add(vehicle2);
     }
 
+    public void setVehicleBuilder(VehicleBuilder vehicleBuilder) { this.builder= vehicleBuilder; }
+    public VehicleBuilder getVehicleBuilder() { return this.builder; }
+
+    public void setVehicles(List<Vehicle> vehicles) { this.vehicles = vehicles; }
     public List<Vehicle> getVehicles() { return this.vehicles; }
+
+    public void setService(VehicleService service) {
+        this.service = service;
+    }
+    public VehicleService getService() {
+        return this.service;
+    }
+
+    public void instantiateDataMock(){
+        this.vehicles.forEach(
+                (Vehicle vehicle) -> this.service.save(vehicle)
+        );
+    }
 }
